cc26e712e8a214b6ead22af7e0c9646820d466b2
adam <adamhc23@gmail.com>
2017-11-20 09:39:47 +0000

    repo created

---
+++ app/src/main/java/com/example/adam/foodtracker/controllers/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.adam.foodtracker.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.adam.foodtracker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
